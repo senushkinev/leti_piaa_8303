517eee067b38d27d86f983ec453d5fdcac759289
Senushkinev <senushkinev@yande.ru>
2020-06-09 02:48:03 +0300

    Fix report and output

--- Senushkin/lab5/CAA_lb5_Aho-Corasick.cpp
+++ Senushkin/lab5/CAA_lb5_Aho-Corasick.cpp
@@ -29,24 +29,40 @@ struct Node{
 
     int h; // Высота вершин
 };
+// abc ab a
 
+void printWay(Node* node) {
+    if (node == nullptr)
+        return;
+    printWay(node->parent);
+    std::cout << node->value;
+}
 
 Node* buildTrie(std::vector<std::string> P) {
+    // создание корня
     Node* root = new Node(nullptr, 'R', 0);
     for(int i = 0; i < (int)P.size(); i++) {
+        // какждый новый шаблон добавляется в бор начиная с корня
         Node* currNode = root;
         for(int j = 0; j < (int)P[i].size(); j++) {
+            // если такой переход уже есть, то просто переходим в следующие вергину
             if(currNode->children[P[i][j]] != nullptr) {
                 currNode = currNode->children[P[i][j]];
             } else {
-                std::cout << "New node: parrent " << currNode->value << " symbol " << P[i][j] << " word length " << currNode->h + 1 << std::endl;
+                // если такого перехода нет, то создаем нового ребенка
+                std::cout << "New node: " << currNode->value << '(';
+                printWay(currNode);
+                std::cout << ')' << std::endl;
                 currNode->children[P[i][j]] = new Node(currNode, P[i][j], currNode->h + 1);
                 currNode = currNode->children[P[i][j]];
             }
         }
+        // помечаем вершины, в которых кончается шаблон
         currNode->terminal = true;
         currNode->p = i + 1;
-        std::cout << "Node " << currNode->value << " is terminal. Word " << P[i] << std::endl;
+        std::cout << "Node " << currNode->value << '(';
+        printWay(currNode);
+        std::cout << ") is terminal. Word " << P[i] << std::endl;
     }
 
     return root;
@@ -56,41 +72,79 @@ Node* buildTrie(std::vector<std::string> P) {
 void createSuffixLink(Node* root) {
     std::queue<Node*> q;
     std::map<char, Node*>::iterator it;
-
+    // кладем в очередь все вершины в которые можно попасть из корня
     for(it = root->children.begin(); it != root->children.end(); it++) {
         if (it->second != nullptr)
             q.push(it->second);
     }
 
     while (!q.empty()) {
+        // считыавем верхнию вершину из очереди
         Node* v = q.front();
         q.pop();
+        // кладем в очередь все вершины, которые можно достичь из текущей вершины
         for(it = v->children.begin(); it != v->children.end(); it++) {
             if (it->second != nullptr)
                 q.push(it->second);
         }
 
+        // переходим по суффиксным ссылкам пока, в какой-нибудь из них не будет перехода
+        // по символу текущей вершины или пока не будет достигнут корень
         char x = v->value;
         Node* p = v->parent;
         p = p->suffixLink;
-        while (p != nullptr && p->children[x] == nullptr)
-            p = p->suffixLink;
+        std::cout << "Parent for current node "<< v->value << '(';
+        printWay(v);
+        std::cout << ") is ";
+        std::cout << v->parent->value << '(';
+        printWay(v->parent);
+        std::cout << ')' << std::endl;
 
+        if (p != nullptr) {
+            std::cout << "Transition the suffix link for this node. ";
+            std::cout << "Current node is " << p->value << '(';
+            printWay(p);
+            std::cout << ')' << std::endl;
+        }
+
+
+
+        while (p != nullptr && p->children[x] == nullptr){
+            std::cout << "Transition the suffix link for this node. ";
+            p = p->suffixLink;
+            if (p != nullptr) {
+                std::cout << "Current node is " << p->value << '(';
+                printWay(p);
+                std::cout << ')' << std::endl;
+            }
+        }
+
+
+        // если суффиксная ссылка указыает на ноль, значит суффиксная ссылка для текущей вершины, должна указывать на корень
         if (p == nullptr) {
             v->suffixLink = root;
-            std::cout << "Suffix link for the node " << v->value << " references to root"  << std::endl;
+            std::cout << "Suffix link for the node " << v->value << '(';
+            printWay(v);
+            std::cout << ") references to root"  << std::endl;
             if (v->terminal) {
                 v->terminalSuffixes.emplace_back(v->h, v->p);
             }
         }
+        // если мы нашли не нулевой суффикс, то суффиксная ссылка должна указывать на эту вершину
         else {
+            std::cout << "Managed to find non-zero suffix ";
+            printWay(p->children[x]);
+            std::cout << std::endl;
             v->suffixLink = p->children[x];
             v->terminalSuffixes = p->children[x]->terminalSuffixes;
-            std::cout << "Suffix link for the node " << v->value << " references to " << v->suffixLink->value << std::endl;
+            std::cout << "Suffix link for the node " << v->value << '(';
+            printWay(v);
+            std::cout << ") references to " << v->suffixLink->value << std::endl;
             if (v->terminal) {
                 v->terminalSuffixes.emplace_back(v->h, v->p);
             }
         }
+        std::cout << std::endl;
     }
 }
 
@@ -100,23 +154,34 @@ std::vector<std::pair<int, int>> findInText(Node* root, const std::string& T, st
 
     Node* currNode = root;
     for(int i = 0; i < (int)T.size(); i++) {
-        std::cout << "Curr symbol is " << T[i] << std::endl;
+        std::cout << "Curr symbol is " << T[i] << '(' << i << ')' << std::endl;
+        // если мы можем перейти считанному символу, то переходим
         if (currNode->children[T[i]] != nullptr) {
-            std::cout << "Transition is possible" << std::endl;
+            std::cout << "Transition is possible. Curr node is " << currNode->children[T[i]]->value << '(';
+            printWay(currNode->children[T[i]]);
+            std::cout << ')' << std::endl;
             currNode = currNode->children[T[i]];
+            // если вершина терминальная, то запоминает индекс вхождения шаблона в текст и индекс шаблона в массиве шаблонов
             for(int j = 0; j < (int)currNode->terminalSuffixes.size(); j++) {
                 std::cout << "Find pattern. Index pattern in text " << i - currNode->terminalSuffixes[j].first + 2 << " Patternt: " << P[ currNode->terminalSuffixes[j].second - 1] << std::endl;
                 foundString.emplace_back(i - currNode->terminalSuffixes[j].first + 2, currNode->terminalSuffixes[j].second);
                 currNode = root;
                 break;
             }
+            // если переход невозможен, то переходим по суффиксной ссылки
         } else {
-            std::cout << "Transition is not possible, use suffix link" << std::endl;
             while (currNode->children[T[i]] == nullptr && currNode != root) {
                 currNode = currNode->suffixLink;
+                std::cout << "Transition is not possible, use suffix link" << std::endl;
+                std::cout << "Curr node is " << currNode->value << '(';
+                printWay(currNode);
+                std::cout << ')' << std::endl;
             }
             if (currNode->children[T[i]] != nullptr) {
                 i--;
+                std::cout << "No null suffix was found" << std::endl;
+            } else {
+                std::cout << "The suffix was not found, we are at the root" << std::endl;
             }
         }
     }
@@ -143,7 +208,7 @@ int main() {
     Node* root = buildTrie(P);
     std::cout << std::endl;
 
-    std::cout << "Create suffix link." << std::endl;
+    std::cout << "Create suffix link. R - mean root" << std::endl;
     // Построение суффиксных ссылок
     createSuffixLink(root);
     std::cout << std::endl;
--- Senushkin/lab5/CAA_lb5_correct_point_search.cpp
+++ Senushkin/lab5/CAA_lb5_correct_point_search.cpp
@@ -32,23 +32,40 @@ struct Node{
 };
 
 
+void printWay(Node* node) {
+    if (node == nullptr)
+        return;
+    printWay(node->parent);
+    std::cout << node->value;
+}
+
+
 Node* buildTrie(std::vector<std::string>& P, std::vector<int>& l) {
+    // создание корня
     Node* root = new Node(nullptr, 'R', 0);
     for(int i = 0; i < (int)P.size(); i++) {
+        // какждая новая подстрока добавляется в бор начиная с корня
         Node* currNode = root;
         for(int j = 0; j < (int)P[i].size(); j++) {
+            // если такой переход уже есть, то просто переходим в следующие вергину
             if(currNode->children[P[i][j]] != nullptr) {
                 currNode = currNode->children[P[i][j]];
             } else {
-                std::cout << "New node: parrent " << currNode->value << " symbol " << P[i][j] << " word length " << currNode->h + 1 << std::endl;
+                // если такого перехода нет, то создаем нового ребенка
+                std::cout << "New node: " << currNode->value << '(';
+                printWay(currNode);
+                std::cout << ')' << std::endl;
                 currNode->children[P[i][j]] = new Node(currNode, P[i][j], currNode->h + 1);
                 currNode = currNode->children[P[i][j]];
             }
         }
+        // помечаем вершины, в которых подстроки кончаются и сохроняем индекс с которого начинается подстрока в шаблоне
         currNode->terminal = true;
         currNode->p = i + 1;
         currNode->arr.emplace_back(l[i]);
-        std::cout << "Node " << currNode->value << " is terminal. Word " << P[i] << std::endl;
+        std::cout << "Node " << currNode->value << '(';
+        printWay(currNode);
+        std::cout << ") is terminal. Word " << P[i] << std::endl;
     }
     return root;
 }
@@ -57,37 +74,71 @@ Node* buildTrie(std::vector<std::string>& P, std::vector<int>& l) {
 void createSuffixLink(Node* root) {
     std::queue<Node*> q;
     std::map<char, Node*>::iterator it;
-
+    // кладем в очередь все вершины в которые можно попасть из корня
     for(it = root->children.begin(); it != root->children.end(); it++) {
         if (it->second != nullptr)
             q.push(it->second);
     }
 
     while (!q.empty()) {
+        // считыавем верхнию вершину из очереди
         Node* v = q.front();
         q.pop();
+        // кладем в очередь все вершины, которые можно достичь из текущей вершины
         for(it = v->children.begin(); it != v->children.end(); it++) {
             if (it->second != nullptr)
                 q.push(it->second);
         }
 
+        // переходим по суффиксным ссылкам пока, в какой-нибудь из них не будет перехода
+        // по символу текущей вершины или пока не будет достигнут корень
         char x = v->value;
         Node* p = v->parent;
         p = p->suffixLink;
-        while (p != nullptr && p->children[x] == nullptr)
-            p = p->suffixLink;
+        std::cout << "Parent for current node "<< v->value << '(';
+        printWay(v);
+        std::cout << ") is ";
+        std::cout << v->parent->value << '(';
+        printWay(v->parent);
+        std::cout << ')' << std::endl;
 
+        if (p != nullptr) {
+            std::cout << "Transition the suffix link for this node. ";
+            std::cout << "Current node is " << p->value << '(';
+            printWay(p);
+            std::cout << ')' << std::endl;
+        }
+
+        while (p != nullptr && p->children[x] == nullptr){
+            p = p->suffixLink;
+            std::cout << "Transition the suffix link for this node. ";
+            if (p != nullptr) {
+                std::cout << "Current node is " << p->value << '(';
+                printWay(p);
+                std::cout << ')' << std::endl;
+            }
+        }
+
+        // если суффиксная ссылка указыает на ноль, значит суффиксная ссылка для текущей вершины, должна указывать на корень
         if (p == nullptr) {
             v->suffixLink = root;
-            std::cout << "Suffix link for the node " << v->value << " references to root"  << std::endl;
+            std::cout << "Suffix link for the node " << v->value << '(';
+            printWay(v);
+            std::cout << ") references to root"  << std::endl;
             if (v->terminal) {
                 v->terminalSuffixes.emplace_back(v->h, v->arr);
             }
         }
+            // если мы нашли не нулевой суффикс, то суффиксная ссылка должна указывать на эту вершину
         else {
+            std::cout << "Managed to find non-zero suffix ";
+            printWay(p->children[x]);
+            std::cout << std::endl;
             v->suffixLink = p->children[x];
             v->terminalSuffixes = p->children[x]->terminalSuffixes;
-            std::cout << "Suffix link for the node " << v->value << " references to " << v->suffixLink->value << std::endl;
+            std::cout << "Suffix link for the node " << v->value << '(';
+            printWay(v);
+            std::cout << ") references to " << v->suffixLink->value << std::endl;
             if (v->terminal) {
                 v->terminalSuffixes.emplace_back(v->h, v->arr);
             }
@@ -101,10 +152,14 @@ std::vector<int> findInText(Node* root, const std::string& T, int patternSize, s
 
     Node* currNode = root;
     for(int i = 0; i < (int)T.size(); i++) {
-        std::cout << "Curr symbol is " << T[i] << std::endl;
+        std::cout << "Curr symbol is " << T[i] << '(' << i << ')' << std::endl;
+        // если мы можем перейти считанному символу, то переходим
         if (currNode->children[T[i]] != nullptr) {
-            std::cout << "Transition is possible" << std::endl;
+            std::cout << "Transition is possible. Curr node is " << currNode->children[T[i]]->value << '(';
+            printWay(currNode->children[T[i]]);
+            std::cout << ')' << std::endl;
             currNode = currNode->children[T[i]];
+            // если вершина терминальная, то высчитываем возможное место появление шаблона в тексте
             std::vector<std::pair<int, std::vector<int>>> terSuff = currNode->terminalSuffixes;
             for(int j = 0; j < (int)terSuff.size(); j++) {
                 std::cout << "Find pattern. Index pattern in text " << i - currNode->terminalSuffixes[j].first + 2 << " Patternt: " << P[currNode->p - 1] << std::endl;
@@ -112,18 +167,27 @@ std::vector<int> findInText(Node* root, const std::string& T, int patternSize, s
                    int curr_i = i - (int)terSuff[j].second[k] + 2 - (int)terSuff[j].first;
                    if (curr_i >= 0 && curr_i + patternSize <= T.size())
                    {
+                       std::cout << "Perhaps the template is located at this index "<< curr_i << ". One of his substrings was discovered" << std::endl;
                        C[curr_i]++;
                    }
                }
 
             }
+            // если переход невозможен, то переходим по суффиксной ссылки
         } else {
             std::cout << "Transition is not possible, use suffix link" << std::endl;
             while (currNode->children[T[i]] == nullptr && currNode != root) {
                 currNode = currNode->suffixLink;
+                std::cout << "Transition is not possible, use suffix link" << std::endl;
+                std::cout << "Curr node is " << currNode->value << '(';
+                printWay(currNode);
+                std::cout << ')' << std::endl;
             }
             if (currNode->children[T[i]] != nullptr) {
                 i--;
+                std::cout << "No null suffix was found" << std::endl;
+            } else {
+                std::cout << "The suffix was not found, we are at the root" << std::endl;
             }
         }
     }
